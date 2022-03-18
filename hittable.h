a18d24044fb45eb79cb5dec633b80bd261d6d71f
Kenny-syc <272703340@163.com>
2022-03-18 21:34:13 +0800

    3.18 write to 7.1

---
+++ hittable.h
@@ -0,0 +1,25 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+struct hit_record
+{
+    point3 p;           //终点
+    vec3 normal;        //单位圆上的坐标
+    double t;           //时间
+    bool front_face;
+
+    inline void set_face_normal(const ray& r,const vec3& outward_normal){
+        front_face = dot(r.direction(),outward_normal)<0;                       //入射光线与表面法线点积
+        normal = front_face ? outward_normal : -outward_normal;                 //>0同向:inside,<0反向:outside
+    }
+};
+
+class hittable{
+    public:
+        virtual bool hit(const ray& r,double t_min,double t_max,hit_record& rec) const=0;
+};
+
+
+#endif
---
+++ ray.h
@@ -0,0 +1,29 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray               //射线（向量）
+{
+
+public:
+    ray(){};
+    ray(const point3& origin,const vec3& direction)
+        : orig(origin),dir(direction)
+    {}
+
+    point3 origin() const{return orig;}
+    vec3 direction() const{return dir;}
+
+    point3 at(double t)const{
+        return orig + t * dir;                  //终点
+    }
+
+
+public:
+    point3 orig;        //点
+    vec3 dir;           //方向
+};
+
+
+#endif
